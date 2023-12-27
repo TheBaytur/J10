4cd6d128939fca51f1e11ab59ecc94970ba0d3b9
TheBaytur <bayturcodding@gmail.com>
2023-12-27 21:39:00 +0800

    StringDemo5

---
+++ src/StringDemo5.java
@@ -0,0 +1,16 @@
+public class StringDemo5 {
+    public static void main(String[] args) {
+        System.out.println("Foobar".endsWith("bar"));
+        System.out.println("Foobar".startsWith("Foo"));
+        System.out.println("Foobar".contains("oo"));
+        System.out.println("Hello world!!!".regionMatches(6, "world", 0, 3));
+    }
+}
+
+// Метод regionMatches() используется для сравнения подстроки в исходной строке
+// с подстрокой в строке параметре.
+// Метод startsWith() проверяет, начинается ли данная подстрока фрагментом,
+// переданным методу в качестве параметра.
+// Метод endsWith() проверяет, совпадает ли с параметром конец строки.
+// Метод contains() проверяет, входит ли подстрока в строку.
+// Рассмотрим пример:
