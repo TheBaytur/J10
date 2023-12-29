7cc9c0407b189e8d88ff7cdfed1d7999d87afbfa
TheBaytur <bayturcodding@gmail.com>
2023-12-29 21:56:54 +0800

    Метод isEmpty()

--- src/StringEmpty.java
+++ src/StringEmpty.java
@@ -5,3 +5,6 @@ public class StringEmpty {
         System.out.println(" ".isBlank());
     }
 }
+
+// Метод isEmpty() проверяет пустая ли строка. Возвращает true, только если длина строки равна 0
+// Метод isBlank() возвращает true, если строка пуста или содержит только пробелы (Java 11)
