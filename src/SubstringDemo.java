5f76e01b8bec6dcbb95a8b7261c50df9185cf18a
TheBaytur <bayturcodding@gmail.com>
2023-12-29 21:50:12 +0800

    SubstringDemo

---
+++ src/SubstringDemo.java
@@ -0,0 +1,6 @@
+public class SubstringDemo {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Moscow!!!".substring(10));
+        System.out.println("Welcome to Moscow!!!".substring(10, 18));
+    }
+}
