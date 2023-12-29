3be7972125e53c6693765660dfa738ca9fbde5bc
TheBaytur <bayturcodding@gmail.com>
2023-12-29 21:53:54 +0800

    StringJoinDemo

---
+++ src/StringJoinDemo.java
@@ -0,0 +1,6 @@
+public class StringJoinDemo {
+    public static void main(String[] args) {
+        String result = String.join(":", "foobar", "foo", "bar");
+        System.out.println(result);
+    }
+}
