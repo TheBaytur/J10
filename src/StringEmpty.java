5129f3221e43df089da4f5a2fe7e4d94b5ac8fa9
TheBaytur <bayturcodding@gmail.com>
2023-12-29 21:56:09 +0800

    StringEmpty

---
+++ src/StringEmpty.java
@@ -0,0 +1,7 @@
+public class StringEmpty {
+    public static void main(String[] args) {
+        System.out.println("".isEmpty());
+        System.out.println(" ".isEmpty());
+        System.out.println(" ".isBlank());
+    }
+}
