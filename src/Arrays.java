59da9c4680ac670b5857faa1285e8f7e3786ab2d
TheBaytur <bayturcodding@gmail.com>
2024-05-29 23:08:21 +0800

    Arrays

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,17 +1,16 @@
-import java.util.ArrayDeque;
+import java.util.ArrayDeque; // Modified Arrays
 
 class Arrays {
     public static void main(String[] args) {
         ArrayDeque<String> animals= new ArrayDeque<>();
+        // Using offer()
+        animals.offer("Dog");
 
-        // Using add()
-        animals.add("Dog");
+        // Using offerFirst()
+        animals.offerFirst("Cat");
 
-        // Using addFirst()
-        animals.addFirst("Cat");
-
-        // Using addLast()
-        animals.addLast("Horse");
+        // Using offerLast()
+        animals.offerLast("Horse");
         System.out.println("ArrayDeque: " + animals);
     }
 }
